71bd4221c9ddfb8aa2e19ec17a9e50b331b1f462
Mendel9331 <110141295+LingErYao@users.noreply.github.com>
2025-09-01 09:44:10 +0800

    替换文件

---
+++ mechanism/src/com/Ling_eryao/operaData/OperaData.java
@@ -0,0 +1,7 @@
+package com.Ling_eryao.operaData;
+
+public class OperaData {
+    public int dir;  // 操纵寻路者的方向：0上1下2左3右
+    public int dis;  //操纵寻路者的距离：单位：格
+    public int totalFrames = 0;  // 动画持续帧数
+}
