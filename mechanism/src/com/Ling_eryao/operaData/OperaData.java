0e687c6dc222d1e121728910b07f0ccf531b0024
Mendel9331 <110141295+LingErYao@users.noreply.github.com>
2025-09-01 09:41:24 +0800

    Delete mechanism directory
    替换文件

--- mechanism/src/com/Ling_eryao/operaData/OperaData.java
+++
@@ -1,7 +0,0 @@
-package com.Ling_eryao.operaData;
-
-public class OperaData {
-    public int dir;  // 操纵寻路者的方向：0上1下2左3右
-    public int dis;  //操纵寻路者的距离：单位：格
-    public int totalFrames = 0;  // 动画持续帧数
-}
